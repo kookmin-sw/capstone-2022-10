36ed666460984c53206d51a5cd8abe0a5fa5d107
Junyong <jypark93@kookmin.ac.kr>
2022-04-11 23:26:48 +0900

    update:YOLO

--- YOLO/YOLOv3.py
+++ YOLO/YOLOv3.py
@@ -9,13 +9,14 @@ height = 0
 show_ratio = 1.0
 
 path = "YOLO/"
-file_name = path + "images/1.jpg" ## 또는 path + "images/2.jpeg" ##  
-Weights = path + 'weights/custom-train-yolo_12000.weights'
+file_name = path + "images/test.jpg"
+Weights = path + 'weights/custom-train-yolo_final.weights'
 test_cfg = path + "cfg/custom-test-yolo.cfg"
 net = cv2.dnn.readNetFromDarknet(test_cfg,Weights)
 
 
 classes = []
+class_count = 50
 anw = []
 with open(path + "classes.txt" , "r") as f:
 	classes = [line.strip() for line in f.readlines()]
@@ -23,9 +24,8 @@ print(classes)
 color_lists = np.random.uniform(0, 255, size= (len(classes), 3))
 
 layer_names = net.getLayerNames()
-# print(layer_names)
-output_layers = [layer_names[i[0] -1] for i in net.getUnconnectedOutLayers()]
-# print(net.getUnconnectedOutLayers())
+#print(layer_names)
+output_layers = ['yolo_82', 'yolo_94', 'yolo_106']
 print(output_layers)
 
 img = cv2.imread(file_name)
@@ -84,7 +84,7 @@ for i in range(len(boxes)):
     #print (type(con))
 		color = colors[i]
 		#print(i, label, color, x, y, w, h)
-		cv2.rectangle(img, (x, y), (x+w, y+h), color, 2)
+		cv2.rectangle(img, (x, y), (x+w, y+h), color, 3)
 		cv2.putText(img, con + "%", (x, y +80), font, 3, color, 3)
 		cv2.putText(img, label, (x, y + 30), font, 3, color, 3)
   		
