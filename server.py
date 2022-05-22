83d00a8e34788d3536996a5a1725b748253d1f77
Hailey Lee <85275893+Sehee-Lee-01@users.noreply.github.com>
2022-05-22 16:26:43 +0900

    modify cors

--- server.py
+++ server.py
@@ -164,7 +164,7 @@ app = FastAPI()
 origins = ["*"]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
